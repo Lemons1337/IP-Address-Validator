51ec55a83dd2fbb88a3be58745bf981879fb6de2
Lemons <24715897+Lemons1337@users.noreply.github.com>
2019-12-08 20:45:58 -0500

    Create index_2.js

---
+++ index_2.js
@@ -0,0 +1,23 @@
+function isValid(ip) {
+    try {
+        var parts = ip.split('.');
+
+        var min = 0x00000001;
+        var max = 0xFFFFFFFF;
+
+        if (parts.length !== 4) return false;
+
+        var hex = parts.map(num => {
+            if (num > 255 || num < 0) {
+                return max.toString(16);
+            }
+            return (+num).toString(16);
+        }).join('');
+
+        hex = parseInt(hex, 16);
+
+        return hex > min && hex < max;
+    } catch (err) {
+        return false;
+    }
+}
